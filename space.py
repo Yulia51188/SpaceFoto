dbed3799cb1b192fd852c3406a95912f2baf2829
Yulia51188 <sun51188@gmail.com>
2019-01-30 16:04:12 +0300

    Add download from SpaceX
    Add code to the main() that make get request to spacex API, get images urls from the latest launch and save to the local folder

--- space.py
+++ space.py
@@ -12,12 +12,22 @@ def save_image_as_file_in_folder(image, folder_name='images', image_filename='im
 
 def download_image(image_url, image_filename):
     response = requests.get(image_url)
+    if not response.ok:
+        return('Request error: {0}'.format(response.text))
     save_image_as_file_in_folder(response.content, image_filename=image_filename)
+    return('Image saved as ../images/{0}'.format(image_filename))
 
 
 def main():
-    url = 'https://upload.wikimedia.org/wikipedia/commons/3/3f/HST-SM4.jpeg'
-    download_image(url, 'hubble.jpeg')
+    spacex_url = 'https://api.spacexdata.com/v3/launches/latest'
+    response = requests.get(spacex_url)
+    if not response.ok:
+    	exit('Request error: {0}'.format(response.text))
+    response_json = response.json()
+    image_urls = response_json["links"]["flickr_images"]
+    for image_index, image_url in enumerate(image_urls, 1):
+        image_filename = 'space{number}.jpeg'.format(number=image_index)
+        print(download_image(image_url, image_filename))
 
 
 if __name__ == '__main__':
