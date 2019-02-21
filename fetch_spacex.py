44a7362a2c2f27654a0c0afd9fe8eb4873347d56
Yulia51188 <sun51188@gmail.com>
2019-02-21 23:09:12 +0300

    Debug fetch and download spacex with names

--- fetch_spacex.py
+++ fetch_spacex.py
@@ -8,14 +8,23 @@ def fetch_spacex_photo_latest_launch(
     response = requests.get(spacex_url)
     response.raise_for_status()
     response_json = response.json()
-    image_urls = response_json["links"]["flickr_images"]	
+    image_urls = [
+        {
+            'url':image_url, 
+            'name': '{name}_{index}'.format(name=response_json['mission_name'], 
+                                            index=index)
+        } 
+        for index, image_url in enumerate(
+            response_json['links']['flickr_images'], 1)
+    ]
+    print(image_urls)
     return image_urls
 
 
 def main():
     try:
         image_urls = fetch_spacex_photo_latest_launch()
-        download_images.download_images_by_urls(image_urls, 'SpaceX')
+        download_images.download_images_by_urls_and_names(image_urls)
     except requests.exceptions.HTTPError as error:
         exit("Can't get data from server:\n{0}".format(error))
 
