0e48c7f6d27890c35291919962670cffb437f9ce
Yulia51188 <sun51188@gmail.com>
2019-02-25 23:44:37 +0300

    Complete fetch_nasa.py

--- fetch_nasa.py
+++ fetch_nasa.py
@@ -3,6 +3,8 @@ import requests
 import argparse
 from dotenv import load_dotenv
 import os
+from datetime import date, timedelta
+from itertools import count
 
 
 def parse_arguments():
@@ -10,36 +12,46 @@ def parse_arguments():
         description='Fetch image url list by NASA API'
     )
     parser.add_argument(
-        '-n', '--count',
+        '-p', '--period',
         type=int,
-        default=2,
-        help='count of photos'
+        default=0,
+        help='period for images publication one per day'
     )  
     return parser.parse_args()
 
 
-def fetch_nasa_image_of_day(
-    key,
-    nasa_url='https://api.nasa.gov/planetary/apod',
-):
+def fetch_nasa_image_of_day(key, publication_date, nasa_url):
     payload = {
         'api_key': key,
-        'date': '2019-01-01'
+        'date': publication_date,
     }
     response = requests.get(nasa_url, params=payload)
     response.raise_for_status()
     image = {}
-    image["url"] = response.json()["hdurl"]
+    image["url"] = response.json()["url"]
     image["name"] = response.json()["title"] 
     return image
+
+
+def fetch_nasa_images_for_period(key, period=0, 
+                            nasa_url='https://api.nasa.gov/planetary/apod'):
+    current_date = date.today()
+    for delta in count(start=0, step=-1):
+        day = current_date + timedelta(days=delta)
+        yield fetch_nasa_image_of_day(key, day, nasa_url)
+        if abs(delta)>=period:
+            break
+    
 
 def main():
     args = parse_arguments()
     load_dotenv()
     nasa_key = os.getenv("NASA_KEY")
     try:
-        image_urls = [(fetch_nasa_image_of_day(nasa_key))]
-        print(image_urls)
+        image_urls = list(fetch_nasa_images_for_period(
+            nasa_key,
+            period=args.period,
+        ))
         download_images.download_images_by_urls_and_names(image_urls)
     except requests.exceptions.HTTPError as error:
         exit("Can't get data from server:\n{0}".format(error))
